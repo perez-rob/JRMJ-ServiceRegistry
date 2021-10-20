d875db231c4285c5654bc392d388f8872842cb3e
bob.w.perez <bob.w.perez@gmail.com>
2021-10-20 14:54:37 -0400

    ok

--- src/main/java/com/jrmj/ServiceRegistry/ServiceRegistryApplication.java
+++ src/main/java/com/jrmj/ServiceRegistry/ServiceRegistryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
-
+	//test comment
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
